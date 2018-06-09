427abd3daed565c2fb5d55a95e4bf93ae15f5ea5
noSinoIlyna <ray0131@jcom.zaq.ne.jp>
2018-06-09 16:42:54 +0900

    Initial Commit

---
+++ effecter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  effecter
+//
+//  Created by 中村柊瑕 on 2018/06/09.
+//  Copyright © 2018年 柊斗 中村. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
